e22a34f2a20d3f6786175d7b1644c5829c41cf70
Kriz Ferre Fajardo <kriz.fajardo@johngaltventures.com>
2018-03-30 21:20:07 +0800

    Initial version

---
+++ js/lists.js
@@ -0,0 +1,39 @@
+var colors = ["bg-color1", "bg-color2", "bg-color3", "bg-color4"];
+
+var quotes = [
+  {
+    quote: "Don't cry because it's over, smile because it happened.",
+    source: "Dr. Seuss",
+    tags: ["cry", "crying", "experience", "happiness", "joy", "life",
+    "optimism", "sadness", "smile", "smiling"]
+  },
+  {
+    quote: "I'm selfish, impatient and a little insecure. I make mistakes, " +
+    "I am out of control and at times hard to handle. But if you can't handle " +
+    "me at my worst, then you sure as hell don't deserve me at my best.",
+    source: "Marilyn Monroe",
+    tags: ["best", "life", "love", "mistakes", "truth", "worst"]
+  },
+  {
+    quote: "Two things are infinite: the universe and human stupidity; " +
+    "and I'm not sure about the universe.",
+    source: "Albert Eintein",
+    tags: ["human-nature", "humor", "infinity", "philosophy", "science",
+    "stupidity", "universe"]
+  },
+  {
+    quote: "There is some good in this world, and it’s worth fighting for.",
+    source: "J.R.R. Tolkien",
+    citation: "The Two Towers",
+    year: 2009,
+    tags: ["adventure", "fantasy", "mystery"]
+  },
+  {
+    quote: "It is better to be hated for what you are than to be loved for " +
+    "what you are not.",
+    source: "André Gide",
+    citation: "Autumn Leaves",
+    year: 1950,
+    tags: []
+  }
+];
---
+++ js/script.js
@@ -0,0 +1,64 @@
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", execute, false);
+
+// set the interval to change the quotes automatically after 5 seconds
+var interval = setInterval(execute, 5000);
+
+// selects a random background color from the bgColors array
+// returns the randomly selected background color
+function getRandomColor() {
+  return Math.floor(Math.random() * colors.length);
+}
+
+// selects a random quote object from the quotes array
+// returns the randomly selected quote object
+function getRandomQuote() {
+  return Math.floor(Math.random() * quotes.length);
+}
+
+// sets the background color
+var oldColor;
+function setBgColor() {
+  var randomColorIndex = getRandomColor();
+  var newColor = colors[randomColorIndex];
+  document.getElementById('body').classList.remove(oldColor);
+  document.getElementById('body').classList.add(newColor);
+  document.getElementById('loadQuote').classList.remove(oldColor);
+  document.getElementById('loadQuote').classList.add(newColor);
+  oldColor = newColor;
+}
+
+// calls the getRandomQuote function and stores the returned quote object in a variable
+// doesn't add a <span class="citation"> for a missing citation or a <span class="year"> if the year property is missing
+// displays the final HTML string to the page
+function printQuote() {
+  var randomQuoteIndex = getRandomQuote();
+  var randomQuote = quotes[randomQuoteIndex];
+  var htmlString =
+    '<p class="quote">' + randomQuote['quote'] + '</p>' +
+    '<p class="source">' + randomQuote['source'];
+
+  if (typeof randomQuote['citation'] !== "undefined") {
+    htmlString += '<span class="citation">' + randomQuote['citation'] + '</span>';
+  }
+
+  if (typeof randomQuote['year'] !== "undefined") {
+    htmlString += '<span class="year">' + randomQuote['year'] + '</span>';
+  }
+
+  htmlString += '</p>';
+
+  if (randomQuote['tags'].length > 0) {
+    htmlString += '<p class="tags">tags: ' + randomQuote['tags'].join(', ') + '</p>';
+  }
+
+  document.getElementById('quote-box').innerHTML = htmlString;
+}
+
+function execute() {
+  setBgColor();
+  printQuote();
+}
+
+execute();
